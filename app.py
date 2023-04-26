f4b6c38b478289a7e9a845da340be5176d9c142e
Wellington Luiz de Faria <91472871+WellingtonLFaria@users.noreply.github.com>
2023-04-26 17:16:19 -0300

    Aplicação WEB para o validador de cpf criada.

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template, request
+from validador_de_cpf import validador
+
+app = Flask(__name__)
+
+@app.route('/', methods=["GET", "POST"])
+def index():
+    if request.method == "GET":
+        return render_template('index.html')
+    else:
+        return render_template("index.html", validacao=validador(int(request.form['cpf']))) if request.form['cpf'] != "" else render_template("index.html", validacao="Campo Vazio")
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
--- validador_de_cpf.py
+++ validador_de_cpf.py
@@ -1,25 +1,27 @@
-while True:
-    # CPF
-    cpf = str(input('Informe um CPF: ')).strip().replace('.', '').replace('-', '')
+def validador(cpf):
+    if len(str(cpf)) > 11:
+        return "CPF tem mais que 11 dígitos"
+    elif len(str(cpf)) < 11:
+        return "CPF tem menos que 11 dígitos"
+    else:
+        cpf = str(cpf)
+        multiplicador = 10
+        soma = 0
+        # Fazendo a soma dos dígitos multiplicados para o primeiro dígito
+        for c in range(0, 9):
+            soma += int(cpf[c])*multiplicador
+            multiplicador -= 1
 
-    multiplicador = 10
-    soma = 0
-    # Fazendo a soma dos dígitos multiplicados para o primeiro dígito
-    for c in range(0, 9):
-        soma += int(cpf[c])*multiplicador
-        multiplicador -= 1
+        # Obtendo o primeiro dígito
+        primeiro_digito = (soma * 10)%11 if (soma * 10)%11 <= 9 else 0
 
-    # Obtendo o primeiro dígito
-    primeiro_digito = (soma * 10)%11 if (soma * 10)%11 <= 9 else 0
+        multiplicador = 11
+        soma = 0
+        # Fazendo a soma dos dígitos multiplicados para o segundo dígito
+        for c in range(0, 10):
+            soma += int(cpf[c])*multiplicador
+            multiplicador -= 1
 
-    multiplicador = 11
-    soma = 0
-    # Fazendo a soma dos dígitos multiplicados para o segundo dígito
-    for c in range(0, 10):
-        soma += int(cpf[c])*multiplicador
-        multiplicador -= 1
-
-    # Obtendo o segundo dígito
-    segundo_digito = (soma * 10)%11 if (soma * 10)%11 <= 9 else 0
-
-    print('CPF Válido' if str(primeiro_digito)+str(segundo_digito) == cpf[-2:] else "CPF Inválido")
+        # Obtendo o segundo dígito
+        segundo_digito = (soma * 10)%11 if (soma * 10)%11 <= 9 else 0
+        return 'CPF Válido' if str(primeiro_digito)+str(segundo_digito) == cpf[-2:] else "CPF Inválido"
